fc4f9f5390914c33aed5565ae695be5222b973c3
yunjae-jang <85554019+yunjae-jang@users.noreply.github.com>
2026-08-20 14:20:43 +0900

    Update 고객DB_Spark_연동용.py

--- 고객DB_Spark_연동용.py
+++ 고객DB_Spark_연동용.py
@@ -19,11 +19,11 @@ now_format_day = now.strftime('%d')
 
 def connectDB(db_use):
     mydb = mysql.connector.connect(
-        host="192.168.0.184",
-        user="manager",
-        passwd="Olitcrm!!",
-        database=db_use,
-        auth_plugin="mysql_native_password"
+        host="",
+        user="",
+        passwd="",
+        database=,
+        auth_plugin=""
     )
     mycursor = mydb.cursor()
     return mydb,mycursor
