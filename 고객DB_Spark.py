a1d81413cd45d8e49881bf96b7be8a956375ed7e
yunjae-jang <85554019+yunjae-jang@users.noreply.github.com>
2026-08-20 14:20:16 +0900

    Update 고객DB_Spark.py

--- 고객DB_Spark.py
+++ 고객DB_Spark.py
@@ -13,11 +13,11 @@ from pyspark.sql.functions import monotonically_increasing_id
 
 def connectDB(db_use):
     mydb = mysql.connector.connect(
-        host="192.168.0.184",
-        user="manager",
-        passwd="Olitcrm!!",
-        database=db_use,
-        auth_plugin="mysql_native_password"
+        host="",
+        user="",
+        passwd="",
+        database=,
+        auth_plugin=""
     )
     mycursor = mydb.cursor()
     return mydb,mycursor
