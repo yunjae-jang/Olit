a1f7ad88b081c7b7a80c8633a1cc998365e89a93
yunjae-jang <85554019+yunjae-jang@users.noreply.github.com>
2026-08-20 14:10:49 +0900

    Update DB재구매율대시보드.py

--- DB재구매율대시보드.py
+++ DB재구매율대시보드.py
@@ -14,11 +14,11 @@ from tkinter import filedialog
 
 def connectDB(db_use):
     mydb = mysql.connector.connect(
-        host="192.168.0.184",
-        user="manager",
-        passwd="Olitcrm!!",
-        database=db_use,
-        auth_plugin="mysql_native_password"
+        host="",
+        user="",
+        passwd="",
+        database=,
+        auth_plugin=""
     )
     mycursor = mydb.cursor()
     return mydb,mycursor
