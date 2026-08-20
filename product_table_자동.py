88538c811cc63b75890fae9586f273a507ff00b1
yunjae-jang <85554019+yunjae-jang@users.noreply.github.com>
2026-08-20 14:17:38 +0900

    Update product_table_자동.py

--- product_table_자동.py
+++ product_table_자동.py
@@ -9,11 +9,11 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 def connectDB(db_use):
     mydb = mysql.connector.connect(
-        host="192.168.0.184",
-        user="manager",
-        passwd="Olitcrm!!",
-        database=db_use,
-        auth_plugin="mysql_native_password"
+        host="",
+        user="",
+        passwd="",
+        database=,
+        auth_plugin=""
     )
     mycursor = mydb.cursor()
     return mydb,mycursor
